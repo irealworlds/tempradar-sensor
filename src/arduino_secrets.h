1407a2847b77b3f13e6c440f235ef52f59303d06
mayaaaai <incermaia@yahoo.com>
2023-12-18 18:34:51 +0200

    sensor

---
+++ src/arduino_secrets.h
@@ -0,0 +1,12 @@
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+
+#define SECRET_PASS_SERVER "zumaEt@re72"
+
+#define SECRET_SSID "Maia"
+#define SECRET_PASS "20maia011609"
+
+// #define SECRET_SSID "ireal"
+// #define SECRET_PASS "qror6051"
+
+#endif
