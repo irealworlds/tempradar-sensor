3765a9118decee244233a82b357ae17662e5d27e
Marin Calin <marincalin2551@hotmail.com>
2023-12-18 18:53:20 +0200

    Refactor code for sending sensor readings
    This commit refactors the code for sending sensor readings to improve readability and efficiency. The changes include reordering the #include statements, removing unnecessary comments and blank lines, and modifying the sendSensorReadings() function. The function now properly checks for an empty authToken and uses a bearer header for authentication. It also handles the case where the status code is 401 (Unauthorized) by calling the login() function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <Arduino.h>
-#include <arduino_secrets.h>
-#include <WiFi.h>
-#include <ArduinoHttpClient.h>
 #include <Adafruit_AHTX0.h>
+#include <Arduino.h>
+#include <ArduinoHttpClient.h>
 #include <ArduinoJson.h>
+#include <WiFi.h>
+#include <arduino_secrets.h>
 
 // Sensor information
 String sensorID = "";
@@ -22,209 +22,206 @@ const int port = 80;
 
 // Sensor information & hardware setup
 const int analogPin = 39; // AQI sensor
-Adafruit_AHTX0 aht;        // temperature and humidity sensor
+Adafruit_AHTX0 aht;       // temperature and humidity sensor
 
 WiFiClient wifi;
 int status = WL_IDLE_STATUS;
 
 bool ensureConnection(HttpClient &client) {
-    if (!client.connected()) {
-        Serial.println("Error: Could not connect to server");
-        return false;
-    }
-    return true;
+  if (!client.connected()) {
+    Serial.println("Error: Could not connect to server");
+    return false;
+  }
+  return true;
 }
 
 void setupWiFi() {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, pass);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
 
-    Serial.print("Connecting to WiFi ..");
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print('.');
-        delay(1000);
-    }
-    Serial.println("");
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("");
 
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
 
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 }
 
 void setupSensor() {
-    analogSetAttenuation(ADC_11db);
+  analogSetAttenuation(ADC_11db);
 
-    if (!aht.begin()) {
-        Serial.println("Could not find AHT? Check wiring");
-        while (1)
-            delay(10);
-    }
-    Serial.println("AHT10 or AHT20 found");
+  if (!aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    while (1)
+      delay(10);
+  }
+  Serial.println("AHT10 or AHT20 found");
 }
 
 void login() {
-    HttpClient client(wifi, serverName, port);
+  HttpClient client(wifi, serverName, port);
 
-    Serial.println("Making POST request to login");
+  Serial.println("Making POST request to login");
 
-    DynamicJsonDocument loginJson(1024);
-    loginJson["macAddress"] = macAddress;
-    loginJson["password"] = password;
+  DynamicJsonDocument loginJson(1024);
+  loginJson["macAddress"] = macAddress;
+  loginJson["password"] = password;
 
-    String postData;
-    serializeJson(loginJson, postData);
-    Serial.println(postData);
+  String postData;
+  serializeJson(loginJson, postData);
+  Serial.println(postData);
 
-    client.post("/auth/sessions", "application/json", postData);
+  client.post("/auth/sessions", "application/json", postData);
 
-    if (client.responseStatusCode() == 201) {
-        Serial.println("Login successful");
-        const char *authTokenJSON = client.responseBody().c_str();
-        DynamicJsonDocument doc(1024);
-        deserializeJson(doc, authTokenJSON);
-        
-        
-        sensorID = doc["subject"].as<String>();
-        
-        authToken = doc["accessToken"].as<String>();
-        Serial.print("Sensor ID(login): ");
-        Serial.println(sensorID);
-        Serial.print("Auth token: ");
-        Serial.println(authToken);
-    } else {
-        Serial.println("Login failed");
-        Serial.println(client.responseStatusCode());
-        Serial.println(client.responseBody());
-    }
-    client.stop(); // Close connection
+  if (client.responseStatusCode() == 201) {
+    Serial.println("Login successful");
+    const char *authTokenJSON = client.responseBody().c_str();
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, authTokenJSON);
+
+    sensorID = doc["subject"].as<String>();
+
+    authToken = doc["accessToken"].as<String>();
+    Serial.print("Sensor ID(login): ");
+    Serial.println(sensorID);
+    Serial.print("Auth token: ");
+    Serial.println(authToken);
+  } else {
+    Serial.println("Login failed");
+    Serial.println(client.responseStatusCode());
+    Serial.println(client.responseBody());
+  }
+  client.stop(); // Close connection
 }
 
 void registerSensor() {
-    HttpClient client(wifi, serverName, port);
+  HttpClient client(wifi, serverName, port);
 
-    Serial.println("Registering sensor");
+  Serial.println("Registering sensor");
 
-    DynamicJsonDocument registerJson(1024);
-    registerJson["macAddress"] = macAddress;
-    registerJson["name"] = sensorName;
-    registerJson["password"] = password;
+  DynamicJsonDocument registerJson(1024);
+  registerJson["macAddress"] = macAddress;
+  registerJson["name"] = sensorName;
+  registerJson["password"] = password;
 
-    String postData;
-    serializeJson(registerJson, postData);
-    Serial.println(postData);
+  String postData;
+  serializeJson(registerJson, postData);
+  Serial.println(postData);
 
-    if (client.post("/sensors", "application/json", postData) != 201) {
-        Serial.println("Register failed");
-        Serial.println(client.responseStatusCode());
-        Serial.println("Trying to login");
+  if (client.post("/sensors", "application/json", postData) != 201) {
+    Serial.println("Register failed");
+    Serial.println(client.responseStatusCode());
+    Serial.println("Trying to login");
 
-        login();
-    } else {
-        Serial.println("Register successful");
+    login();
+  } else {
+    Serial.println("Register successful");
 
-        const char *sensorIDJSON = client.responseBody().c_str();
-        DynamicJsonDocument doc(1024);
-        deserializeJson(doc, sensorIDJSON);
-        sensorID = doc["resourceIdentifier"].as<String>();
+    const char *sensorIDJSON = client.responseBody().c_str();
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, sensorIDJSON);
+    sensorID = doc["resourceIdentifier"].as<String>();
 
-        Serial.print("Sensor ID(register): ");
-        Serial.println(sensorID);
-    }
-    client.stop(); // Close connection
+    Serial.print("Sensor ID(register): ");
+    Serial.println(sensorID);
+  }
+  client.stop(); // Close connection
 }
 
-void sendSensorReadings(int aqiIndex, float humidity_f, float temp_f, String sensorID) {
-    Serial.println(sensorID);
-    if (authToken.isEmpty()) {
+void sendSensorReadings(int aqiIndex, float humidity_f, float temp_f,
+                        String sensorID) {
+  Serial.println(sensorID);
+  if (authToken == nullptr || authToken.isEmpty()) {
     Serial.println("Error: Auth token not set");
     return;
-}
+  }
 
-    if (authToken == nullptr) {
-        Serial.println("Error: Auth token not set");
-        return;
-    }
+  HttpClient client(wifi, serverName, port);
 
-    HttpClient client(wifi, serverName, port);
+  // Ensure sensorID is not empty before building the URL
+  String url = "/sensors/" + sensorID + "/readings";
+  const char *url_c_str = url.c_str();
+  Serial.print("\t In sendSensorReadings:");
+  Serial.println(url_c_str);
 
-    // Ensure sensorID is not empty before building the URL
-    String url = "/sensors/" + sensorID + "/readings";
-    const char* url_c_str = url.c_str();
-    Serial.print("\t In sendSensorReadings:");
-    Serial.println(url_c_str);
+  if (strstr(url_c_str, "//readings") != nullptr) {
+    Serial.println("Error: Invalid sensor ID");
+    return;
+  }
 
-    if (strstr(url_c_str, "//readings") != nullptr) {
-        Serial.println("Error: Invalid sensor ID");
-        return;
-    }
+  // Serial.println("\t AuthToken verificare: " + authToken);
+  const char *authToken_c_str = authToken.c_str();
+  const char *bearerHeader_c_str = String("Bearer " + authToken).c_str();
+  Serial.print("\t AuthToken verificare:");
+  Serial.println(bearerHeader_c_str);
 
-    // Serial.println("\t AuthToken verificare: " + authToken);
-    const char *authToken_c_str = authToken.c_str();
-    Serial.print("\t AuthToken verificare:");
-    Serial.println(String("Bearer " + authToken).c_str());
+  client.beginRequest();
+  client.sendHeader("Accept", "application/json");
+  client.sendHeader("Authorization", bearerHeader_c_str);
+  client.sendHeader("Content-Type", "application/json");
 
-    client.sendHeader("Accept", "application/json");
-    client.sendHeader("Authorization", String("Bearer " + authToken).c_str());
-    client.sendHeader("Content-Type", "application/json");
- 
+  Serial.println(authToken);
 
-    Serial.println(authToken);
+  Serial.println("making POST request");
+  DynamicJsonDocument readingJson(1024);
+  readingJson["airQuality"] = aqiIndex;
+  readingJson["humidity"] = humidity_f;
+  readingJson["temperature"] = temp_f;
 
-    Serial.println("making POST request");
-    DynamicJsonDocument readingJson(1024);
-    readingJson["airQuality"] = aqiIndex;
-    readingJson["humidity"] = humidity_f;
-    readingJson["temperature"] = temp_f;
+  String postData;
+  serializeJson(readingJson, postData);
 
-    String postData;
-    serializeJson(readingJson, postData);
+  client.post(url_c_str, "application/json", postData);
 
-    client.post(url_c_str, "application/json", postData);
+  client.endRequest();
 
-    int statusCode = client.responseStatusCode();
-    String response = client.responseBody();
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
 
-    if (statusCode == 401) {
-        Serial.println("Unauthorized");
-        // login();
-    } else {
-      Serial.print("Status code: ");
-      Serial.println(statusCode);
-      Serial.print("Response: ");
-      Serial.println(response);
-    }
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
 
-    client.stop(); // Close connection
+  if (statusCode == 401) {
+    login();
+  }
+
+  client.stop(); // Close connection
 }
 void setup() {
-    Serial.begin(115200);
-    delay(2500);
+  Serial.begin(115200);
+  delay(2500);
 
-    setupWiFi();
-    setupSensor();
+  setupWiFi();
+  setupSensor();
 
-    Serial.println("Registering sensor");
-    registerSensor();
+  Serial.println("Registering sensor");
+  registerSensor();
 
-    Serial.println("Setup done! ✅");
-    Serial.println("--------------------");
+  Serial.println("Setup done! ✅");
+  Serial.println("--------------------");
 }
 
 void loop() {
-    int aqiIndex = analogRead(analogPin);
-    Serial.println(aqiIndex);
+  int aqiIndex = analogRead(analogPin);
+  Serial.println(aqiIndex);
 
-    sensors_event_t humidity, temp;
-    aht.getEvent(&humidity, &temp);
-    float humidity_f = humidity.relative_humidity;
-    float temp_f = temp.temperature;
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+  float humidity_f = humidity.relative_humidity;
+  float temp_f = temp.temperature;
 
-    Serial.println("\t In loop " + String(sensorID));
-    sendSensorReadings(aqiIndex, humidity_f, temp_f,sensorID);
+  Serial.println("\t In loop " + String(sensorID));
+  sendSensorReadings(aqiIndex, humidity_f, temp_f, sensorID);
 
-    Serial.println("Wait 2.5 seconds");
-    delay(2500);
+  Serial.println("Wait 2.5 seconds");
+  delay(2500);
 }
